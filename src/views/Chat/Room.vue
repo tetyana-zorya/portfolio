af7f7f41fb27d4f9a8b8029785bfdc3d3b298933
tetyana-zorya <tetyanazorya@gmail.com>
2021-11-01 16:20:30 +0100

    sends messages

--- src/views/Chat/Room.vue
+++ src/views/Chat/Room.vue
@@ -1,34 +1,44 @@
 <template>
   <div>
     <v-container>
-    <br>
     Welcome to the {{roomData.room_name}} chatroom
-    <br>
     <br>
     members:
     <br>
     <ul v-for="(item, idx) in members" :key="idx" style="list-style-type: none;">
       <li>{{item.username}}</li>
     </ul>
-    <br>
     Access: {{roomData.access}}
     <br>
     Admin: {{roomData.admin}}
+    </v-container>
+
+    <v-container>
+      <v-row justify="center">
+        <v-card style="width:600px; height:500px; border: solid 20px #f5f5f5;">
+          <v-row justify="center">
+            <span style="position:absolute; bottom:.5em;">
+            <v-text-field ref="message" v-model="textInput" placeholder="message" solo rounded style="width:500px;" append-outer-icon="mdi-arrow-right-bold-box" v-on:keyup.enter="send()"></v-text-field>
+          </span>
+          </v-row>
+        </v-card>
+      </v-row>
     </v-container>
     
   </div>
 </template>
 
 <script> 
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, set  } from "firebase/database";
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'chat-room',
   data() {
     return {
-      roomData: '',
-      members: []
+      roomData: [],
+      members: [],
+      textInput: ''
     }
   },
   created() {
@@ -79,6 +89,37 @@ export default {
       console.log(members)
       this.members = members
     },
+
+    send() {
+      console.log(this.textInput)
+      console.log(this.roomData.room_id)
+      console.log(this.getLoggedUser.name)
+      const today = new Date();
+      const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+      const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+      const dateTime = date +' '+ time;
+      console.log(dateTime)
+      console.log(date)
+      console.log(time)
+
+      const db = getDatabase();
+        set(ref(db, 'messages/' + this.roomData.room_id), {
+          message: this.textInput,
+          sender_uid: this.getLoggedUser.uid,
+          sender_name: this.getLoggedUser.name,
+          org: this.roomData.color,
+          members: this.members,
+          chatroom_uid: this.roomData.room_id,
+          chatroom_name: this.roomData.room_name,
+          ts: dateTime,
+          date: date,
+          time: time
+          
+        }).then(() => { console.log('done')}) 
+        .catch((err) => { console.log(err)})
+        console.log('done writing')
+      this.$refs.message.reset()
+    }
 
   },
 
